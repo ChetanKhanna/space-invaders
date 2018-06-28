450cc6f1b0e272ace867c626a10f1de26eb25ecb
chetan22 <ck.chetan20@gmail.com>
2018-06-28 20:12:56 +0530

    Add blocks.

--- space-invaders-oop.py
+++ space-invaders-oop.py
@@ -6,7 +6,7 @@ import random
 BLACK = ( 0, 0, 0)
 WHITE = (255, 255, 255)
 
-screen = pygame.display.set_mode((600,800)) #Initialzing a screen for display
+screen = pygame.display.set_mode((800,600)) #Initialzing a screen for display
 pygame.display.set_caption('Space Invaders')
 
 #List to store enemy ships
@@ -47,17 +47,22 @@ class Defender(pygame.sprite.Sprite):   #Ship was confusing me with the ships []
         
         self.image = pygame.image.load("defender.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = (x_pos, y_pos))
+        pass
         
     def left(self, movement):
         self.rect.x += movement # we will need a limit; if rect.x goes beyond that limit, 
                                 # it will hit the wall or something and hence, won't increase
+        pass
+        
     def right(self, movement):
         self.rect.x -= movement
+        pass
         
     def shoot(self):
-        Missile.shoot() # missile is another class
+       # Missile.shoot() # missile is another class
                         # not sure, still
-
+       pass
+  
 class Bullet(pygame.sprite.Sprite):
     """
     describing bullets; thinking to keep missils
@@ -71,10 +76,12 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = (x_pos, y_pos))
         self.direction = direction
         self.speed = speed
+        pass
         
-    def shoot(self)
+    def shoot(self):
         # if it goes beyond certain dimension, self.kill()
         # self.rect.y += self.speed * self.direction
+        pass
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -87,28 +94,31 @@ class Enemy(pygame.sprite.Sprite):
         super().__init__()
         
         self.image = alien_image
-        self.rect = self.image.get_rect(topleft = (x_pos, y_pos)) 
+        self.rect = self.image.get_rect(topleft = (x_pos, y_pos))
+        pass 
             
     def move_down(self, movement):
         # to make the ship come down
+        pass
         
     def move_horizontal(self, movement): 
         # to make ship oscilate horizontally
+        pass
         
     def shoot(self):
         # not all ships will shoot at once, we need to randomly select some of them
         # and call the Missile.shoot function or something to make them shoot
-
+        pass
 
 class Blocker(pygame.sprite.Sprite):
     """
     Class for defining blocks and their properties;
     basically their damage rate.
     """
-    def __init__(self, block_image):
-        super.()__init__()
-        self.image = block_image
-        self.rect = self.image.get_rect()
+    def __init__(self, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
         # or probably, just a simple rectangle object.
         # for i in range(self.height):
         #     for j in range(self.width):
@@ -120,40 +130,65 @@ class Blocker(pygame.sprite.Sprite):
         # missile hits it. Not really sure how well it will
         # work though
         
+    def draw(self):
+        # pygame.draw.rect(screen, WHITE, [self.x, self.y, 125,  60])
+        
+        # instead of making it single rectangle, distributed each 
+        # rectangle into 25 smaller rectangles. Thought this may
+        # help in collision detection and we can delete smaller rectangles 
+        # when a missile hits it.
+        for i in range(5):
+            for j in range(5):
+                x_pos = self.x + 25*j
+                y_pos = self.y + 12*i
+                pygame.draw.rect(screen, WHITE, [x_pos, y_pos, 25, 12])
+        
     def damage(self):
         # a function to calculate damage to block
+        pass
+        
 
 class Mystery(object):
-	def __init__(self):
+    def __init__(self):
 
-	#Other methods for displaying, updating and images
-
-
+    #Other methods for displaying, updating and images
+        pass
+    
 class Explosion(object):
-	def __init__(self):
+    def __init__(self):
 
     #Other methods for displaying, updating and images
-
+        pass
+    
 class Life(object):
-	def __init__(self):
+    def __init__(self):
 
 	#Other methods for displaying, updating and images
-
+        pass
+    
 class Text(object):
-	def __init__(self):
+    def __init__(self):
 
     #Other methods for displaying, updating and images
-
+        pass
 
 class SpaceInvaders(object):
-	def __init__(self):
+    def __init__(self):
+        pass
 
-	def reset(self):
+    def reset(self):
+        pass
 
 	#Functions for working on Sound, initial, score, displaying etc.
 
-	def main(self):
-
+    def main(self):
+        block_1 = Blocker(75,450)
+        block_2 = Blocker(337.5,450)
+        block_3 = Blocker(600, 450)
+        block_1.draw()
+        block_2.draw()
+        block_3.draw()
+        pygame.display.update()
 
 if __name__ == '__main__':
     game = SpaceInvaders()
