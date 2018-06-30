1211c9d064d6996412c0aa0ab435f6b82f8e43d3
Chetan Khanna <31681374+ChetanKhanna@users.noreply.github.com>
2018-06-30 15:39:35 +0530

    Added "WASD" key support and Minor fixes (#19)
    Add 'WASD' support and Minor fixes

--- space-invaders-oop.py
+++ space-invaders-oop.py
@@ -13,6 +13,7 @@ YELLOW = (241, 255, 0)
 BLUE = (80, 255, 239)
 PURPLE = (203, 0, 255)
 RED = (237, 28, 36)
+ROCK = (138, 51, 36)
 
 # adding font file
 FONT = "fonts/space_invaders.ttf"
@@ -37,35 +38,29 @@ class Ship(pygame.sprite.Sprite):
     # defines the initial x and y pos of our ship
     def __init__(self, x_pos, y_pos):
         super().__init__()
-
-        #self.enemy1 = pygame.image.load("./images/enemy3_1.png").convert_alpha()
-        #self.enemy1 = pygame.transform.scale(self.enemy1 , (40, 40))
-        #self.screen.blit(self.enemy1, (300, 250))
-
         self.image = pygame.image.load("./images/ship.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = (x_pos, y_pos))
-        #self.screen.blit(self.image, (x_pos, y_pos))
         self.moving_speed = 1
-
-    def update(self, screen, keystate):
-        if keystate[pygame.K_RIGHT]:
+        
+    def update(self, keystate):
+        if keystate[pygame.K_RIGHT] or keystate[pygame.K_d]:
             if self.rect.x < 730:
                 self.rect.x += self.moving_speed
 
-        if keystate[pygame.K_LEFT]:
+        if keystate[pygame.K_LEFT] or keystate[pygame.K_a]:
             if self.rect.x > 20:
                 self.rect.x -= self.moving_speed
 
-        self.draw(screen)
+        self.draw()
 
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+    def draw(self):
+       game.screen.blit(self.image, self.rect)
 
     def shoot(self):
-        shoot_sound.play()
-       # Missile.shoot() # missile is another class
-                        # not sure, still
+        # Missile.shoot() # missile is another class
+                         # not sure, still
         pass
+
 
 class Bullet(pygame.sprite.Sprite):
     """
@@ -95,25 +90,21 @@ class Enemy(pygame.sprite.Sprite):
     properties. We need to create a list of
     lists of objects of this class Aliens probably.
     """
-    def __init__(self, ships, alien_image, x_pos, y_pos):
+    def __init__(self, image, x, y):
         super().__init__()
-
-        self.image = alien_image
-        self.rect = self.image.get_rect(topleft = (x_pos, y_pos))
         pass
-
-    def move_down(self, movement):
-        # to make the ship come down
-        pass
-
-    def move_horizontal(self, movement):
-        # to make ship oscilate horizontally
+            
+    def update(self):
+        ### A FUNCTION WHICH MOVES All_Aliens SPRITE GROUP ###
         pass
 
     def shoot(self):
         shoot_sound.play()
         # not all ships will shoot at once, we need to randomly select some of them
         # and call the Missile.shoot function or something to make them shoot
+        pass
+        
+    def draw(self):
         pass
 
 class Blocker(pygame.sprite.Sprite):
@@ -137,8 +128,8 @@ class Blocker(pygame.sprite.Sprite):
             for j in range(5):
                 x_pos = self.x + 25*j
                 y_pos = self.y + 12*i
-                pygame.draw.rect(game.screen, WHITE, [x_pos, y_pos, 25, 12])
-
+                pygame.draw.rect(game.screen, ROCK, [x_pos, y_pos, 25, 12])
+        
     def damage(self):
         pass
 
@@ -186,6 +177,17 @@ class SpaceInvaders(object):
 
         #Initializing font module
         pygame.font.init()
+        
+        #Initialzing high score from text file "highscore.txt"
+        try:
+            filename = "highscore.txt"
+            file = open(filename,"r")
+            self.highest_score = int(file.read())
+            if self.highest_score == ' ':
+                self.highest_score=0
+            file.close()
+        except:
+            self.highest_score=0
 
         #Initialzing high score from text file "highscore.txt"
         try:
@@ -282,21 +284,21 @@ class SpaceInvaders(object):
             self.live = pygame.image.load("./images/ship.png").convert_alpha()
             self.live = pygame.transform.scale(self.live , (20, 20))
             self.screen.blit(self.live, (725+(i*25), 7))
-
-
-    def update_screen(self):
-        self.screen.fill(BLACK)
-        self.new_surface.blit(self.background, (0,0))
-        self.screen.blit(self.new_surface,(0,0))
-
+        
+    def start_game(self):
+        self.background = pygame.image.load("./images/background.png").convert_alpha()
+        self.background = pygame.transform.scale(self.background, (800,600))
+        
+        ## ADD GAMEPLAY AND START SOUND HERE
     def start_game(self):
         self.background = pygame.image.load("./images/background.png").convert_alpha()
         self.background = pygame.transform.scale(self.background, (800,600))
         pygame.mixer.music.stop()
         pygame.mixer.music.load('./sounds/game_sound.wav')
         pygame.mixer.music.play(-1)
+        
         ## ADD GAMEPLAY START SOUND HERE
-
+        
         self.screen.fill(BLACK)
         start_time = time.time()
         end =  False
@@ -305,25 +307,28 @@ class SpaceInvaders(object):
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
-            current_time = time.time() - start_time
-            if current_time <= 3:
-                alpha = (1.0 * current_time / 3)
-
+                    
+            elapsed_time = time.time() - start_time
+            if elapsed_time <= 3:
+                alpha = (1.0 * elapsed_time / 3)
+                
             else:
                 end = True
+                
+            self.background_surface = pygame.surface.Surface((800,600))
+            self.background_surface.set_alpha(255 * alpha)
+            
+            self.screen.fill(BLACK)
+            self.background_surface.blit(self.background, (0,0))
+            self.screen.blit(self.background_surface,(0,0))
 
-            self.new_surface = pygame.surface.Surface((800,600))
-            self.new_surface.set_alpha(255 * alpha)
-
-            self.update_screen()
             pygame.display.flip()
 
         ### ADD all Sprites class object declaration HERE ###
 
         #Defender Ship
         self.player = Ship(375, 530)
-        self.player.draw(self.screen)
+        self.player.draw()
 
         #Blockers
         self.block_1 = Blocker(75,450)
@@ -332,6 +337,8 @@ class SpaceInvaders(object):
         self.block_1.draw()
         self.block_2.draw()
         self.block_3.draw()
+        
+        # Drawing ships                
 
         self.draw_state += 1
 
@@ -349,21 +356,20 @@ class SpaceInvaders(object):
                         self.start_game()
 
             if self.draw_state > 0:
-
                 #Updating Ship's location
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT: #If user quits game
                         quit = True
                 keystate = pygame.key.get_pressed()
-
+                
                 ### CALL All updating functions here ###
                 self.screen.blit(self.background,(0,0))
-                self.player.update(self.screen, keystate)
-                self.block_1.draw()
+                self.player.update(keystate)
+                self.block_1.draw()             # This will need replacement once damage() function is up.
                 self.block_2.draw()
                 self.block_3.draw()
-                self.update_stats()
-
+                self.update_stats()   
+                 
             """ if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
                         #print("Shoot")
@@ -390,7 +396,7 @@ class SpaceInvaders(object):
                 if won == 1:
                     win_message()#Displaying victory message by calling win_message() function
             """
-            pygame.display.flip()
+            pygame.display.flip() #Update portions of the screen for software displays
             #pygame.display.update() #Update portions of the screen for software displays
 
         pygame.quit() #Uninitialize all pygame modules
